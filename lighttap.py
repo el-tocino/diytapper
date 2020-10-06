f4140ec84718dfc8782b08439c8ed34a715b5574
el-tocino <email+github@aforkinthetoad.com>
2020-10-05 23:16:47 -0500

    cleanup

--- lighttap.py
+++ lighttap.py
@@ -1,7 +1,8 @@
+""" DIY  whack-a-mole with led buttons aka fitlights.   """
 #!/usr/bin/python3
 
-import sys
 import time
+import random
 from gpiozero import Button
 from gpiozero import LED
 
@@ -20,11 +21,12 @@ def buttonon(bid):
 
 def buttonoff(bid):
     """ turn off button's LED """
-    buttonled = LED(ledpin[bid])
+    buttonled = LED(ledpins[bid])
     buttonled.off()
 
 
 def alloff():
+    """ Turn off all the LED's """
     for lid in ledpins:
         buttonoff(lid)
 
@@ -42,60 +44,60 @@ def countdown():
     buttonon(1)
     buttonon(0)
     time.sleep(.5)
-    alloff
+    alloff()
     time.sleep(.5)
     buttonon(1)
     buttonon(2)
     buttonon(3)
     time.sleep(.5)
-    alloff
+    alloff()
     time.sleep(.5)
     buttonon(2)
     time.sleep(.5)
-    alloff
+    alloff()
     staticdelay = 0.5
     randomdelay = random.random() + random.random()
     delay = staticdelay + randomdelay
     time.sleep(delay)
 
 
-def logging(buttonid,tsid):
+def logging(buttonid, tsid, logname):
     """ save to log. """
-    print(buttonid, tsid, file=logfile)
+    print(buttonid, tsid, file=open(logname, "a"))
 
 
 def randombutton():
     """ get a random button between 0 and 4"""
-    buttonid = randint(0,4)
-    return(buttonid)
+    buttonid = random.randint(0, 4)
+    return buttonid
 
 
 def getbuttonpress(bid):
     """ Check if button has been pressed yet. If so turn off LED """
     button = Button(buttonpins[bid])
     button.wait_for_press(10)
+    buttonoff(bid)
+    bidtime = time.time()
+    return bidtime
 
-
-def playgame(starttime):
+def playgame(starttime, logname):
     """ run the game. """
     endtime = starttime + gamelen
-    logging("starting",starttime)
+    logging("starting", starttime, logname)
     lightcount = 0
     while time.time() < endtime:
         button = randombutton
         buttonon(button)
-        getbuttonpress(button)
-        lightcount += 1
-        logging(button, time.time())
-    logging("ending",time.time())
-    logging("lightcount",lightcount)
-      
+        tapped = getbuttonpress(button)
+        if tapped <= endtime:
+            lightcount += 1
+        logging(button, time.time(), logname)
+    logging("ending", time.time(), logname)
+    logging("lightcount", lightcount, logname)
 
 def main():
-    countdown
+    """ Do the stuff and things! """
+    countdown()
     starttime = time.time()
     logname = str(int(starttime)) + ".log"
-    logfile = open(logname, "w")
-    playgame(starttime)
-    close(logfile)
-
+    playgame(starttime, logname)
